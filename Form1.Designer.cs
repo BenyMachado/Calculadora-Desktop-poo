fc2ba42c1cc28affa82cd26014d72475b7997cab
BERNARD MACHADO <06006244@acad.unifeso.lan>
2024-09-03 22:17:19 -0300

    3rd commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Calculadora
+{
+    partial class FrmCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtVisor = new System.Windows.Forms.TextBox();
+            this.btnAcao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtVisor
+            // 
+            this.txtVisor.Location = new System.Drawing.Point(164, 40);
+            this.txtVisor.Name = "txtVisor";
+            this.txtVisor.ReadOnly = true;
+            this.txtVisor.Size = new System.Drawing.Size(784, 20);
+            this.txtVisor.TabIndex = 0;
+            // 
+            // btnAcao
+            // 
+            this.btnAcao.Location = new System.Drawing.Point(476, 247);
+            this.btnAcao.Name = "btnAcao";
+            this.btnAcao.Size = new System.Drawing.Size(75, 23);
+            this.btnAcao.TabIndex = 1;
+            this.btnAcao.Text = "Clique Aqui";
+            this.btnAcao.UseVisualStyleBackColor = true;
+            this.btnAcao.Click += new System.EventHandler(this.btnAcao_Click);
+            // 
+            // FrmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 569);
+            this.Controls.Add(this.btnAcao);
+            this.Controls.Add(this.txtVisor);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmCalculadora";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVisor;
+        private System.Windows.Forms.Button btnAcao;
+    }
+}
+
